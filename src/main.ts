16848261471eb86c3cc1ef14486d54df6bb149d3
zhangjh024 <773858072@qq.com>
2023-04-07 10:42:26 +0800

    feat:设置底部导航栏和路由

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from "./router"
 
-import './assets/main.css'
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from 'vue-router'
+import { Tabbar, TabbarItem } from 'vant'
+import { ref, watch } from 'vue'
+
+const route = useRoute()
+const router = useRouter()
+// 记录当前选择的 tab
+const active = ref(route.name as string)
+
+watch(active, (nv) => {
+  router.push({
+    name: nv,
+  })
+})
+</script>
+
+<template>
+  <!-- <div class="center">{{ active }}</div> -->
+  <RouterView />
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact">我的</TabbarItem>
+  </Tabbar>
+</template>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
