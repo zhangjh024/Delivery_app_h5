6aabaf3538e750efe5e3e970804870db40f6654e
zhangjh024 <773858072@qq.com>
2023-04-11 11:24:22 +0800

    feat:postcss实现移动端

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+const rootValue = 16
+const rootWidth = 390
+const deviceWidth = document.documentElement.clientWidth
+document.documentElement.style.fontSize = (deviceWidth * rootValue) / rootWidth + 'px'
 app.mount('#app')
--- src/views/tabs/home/HomeView.vue
+++ src/views/tabs/home/HomeView.vue
@@ -1,3 +1,10 @@
 <template>
-    <div>home</div>
-  </template>
+  <div>home</div>
+  <div class="test">测试测试测试测试测试</div>
+</template>
+
+<style>
+.test {
+  font-size: 39px;
+}
+</style>
